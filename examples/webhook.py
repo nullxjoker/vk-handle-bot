3df6fb98425efdc2b0b9eb7ef7107201f2af1a6e
CodeSQRT <codemasterio@protonmail.com>
2019-03-09 02:19:41 +0300

    add examples

--- examples/webhook.py
+++ examples/webhook.py
@@ -1,6 +1,11 @@
+from flask import Flask, request
 from vk_handle_bot import VkBot
+import json
 
+
+app = Flask(__name__)
 bot = VkBot(token="your token")
+
 
 def start(e):
 	bot.send_message(text=f"Привет, {e.from_id}", peer_id=e.peer_id)
@@ -34,4 +39,15 @@ def getByPhoto(e):
 def unknownCommand(e):
 	bot.send_message(text="Неизвестная команда", peer_id=e.peer_id)
 
-bot.polling(interval=1)
+@app.route('/vk', methods=['GET', 'POST', 'DELETE'])
+def vkontakte():
+	data = json.loads(request.data)
+	if "type" not in data.keys():
+		return "not vk"
+	if data['type'] == "confirmation":
+		return "e0df84a3"
+	elif data['type'] == "message_new":
+		bot.process_new_update(data['object'])
+		return 'ok'
+
+app.run(host="0.0.0.0", debug=True, port=80)
